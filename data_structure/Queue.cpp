2e1aa741b19880da5386e23d1488d9a0b11b5d66
pnuth <85662273+radicalparty@users.noreply.github.com>
2024-05-03 15:16:23 +0900

    Create Queue.cpp

---
+++ data_structure/Queue.cpp
@@ -0,0 +1,36 @@
+template<typename T>
+class Queue{
+    class Node{
+    public:
+        T value;
+        Node* nxt;
+        Node(T value): value(value) {}
+        ~Node(){
+
+        }
+    };
+    Node* fr;
+    Node* ed;
+    int sz;
+public:
+    Queue(): sz(0) {fr = nullptr; ed = nullptr;}
+    void push(T value){
+        Node* p = new Node(value);
+        if (!sz){
+            fr = p; ed = p;
+        }
+        else{
+            ed->nxt = p; ed = ed->nxt;
+        }
+        sz++;
+    }
+    void pop(){
+        if (sz){Node* p = fr; fr = fr->nxt; delete(p);}
+        else{return;}
+        sz--;
+    }
+    int size() {return sz;}
+    int empty() {return ((sz == 0) ? 1 : 0);}
+    int front() {return fr->value;}
+    int back()   {return ed->value;}
+};
