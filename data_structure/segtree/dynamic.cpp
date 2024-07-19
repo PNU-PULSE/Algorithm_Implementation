13088b0b4887fca2af879a5c71740f0e02763c38
pnuth <85662273+radicalparty@users.noreply.github.com>
2024-07-19 23:28:09 +0900

    Update dynamic.cpp

--- data_structure/segtree/dynamic.cpp
+++ data_structure/segtree/dynamic.cpp
@@ -1,29 +1,40 @@
 //reference: https://justicehui.github.io/medium-algorithm/2020/02/28/DynamicSeg/
 struct Node{
     Node *l, *r;
-    ll tsum;//구간 합
-    Node(): l(nullptr), r(nullptr), tsum(0) {}
+    ll value;
+    Node(): l(nullptr), r(nullptr), value(0) {}
 };
-void update(Node *node, int s, int e, int p, ll val){
-    if (s == e){
-        node->tsum = val; return;
+struct segtree{
+    Node *root;
+    segtree(): root(new Node()) {}
+    void up(ll s, ll e, ll p, ll val){
+        update(root, s, e, p, val);
     }
-    int m = (s + e) >> 1;
-    if (p <= m){//l, r을 반으로 나눠서 구간 선택, 나머지는 일반적인 세그와 동일
-        if (!node->l) node->l = new Node();
-        update(node->l, s, m, p, val);
+    ll qu(ll s, ll e, ll l, ll r){
+        return query(root, s, e, l, r);
     }
-    else{
-        if (!node->r) node->r = new Node();
-        update(node->r, m + 1, e, p, val);
+    void update(Node *node, ll s, ll e, ll p, ll val){
+        if (s == e){
+            node->value = val; return;
+        }
+        ll m = (s + e) >> 1;
+        if (p <= m){
+            if (!node->l) node->l = new Node();
+            update(node->l, s, m, p, val);
+        }
+        else{
+            if (!node->r) node->r = new Node();
+            update(node->r, m + 1, e, p, val);
+        }
+        ll lt = (node->l) ? node->l->value : 0;
+        ll rt = (node->r) ? node->r->value : 0;
+        node->value = lt + rt;
     }
-    ll left_t = (node->l) ? node->l->tsum : 0;
-    ll right_t = (node->r) ? node->r->tsum : 0;
-    node->tsum = left_t + right_t;
-}
-ll query(Node *node, int s, int e, int le, int ri){
-    if (!node || ri < s || e < le) return 0;
-    if (le <= s && e <= ri) return node->tsum;
-    int m = (s + e) >> 1;
-    return query(node->l, s, m, le, ri) + query(node->r, m + 1, e, le, ri);
-}
+    ll query(Node *node, ll s, ll e, ll l, ll r){
+        if (!node || e < l || r < s) return 0;
+        if (l <= s && e <= r) return node->value;
+        ll m = (s + e) >> 1;
+        return query(node->l, s, m, l, r) +
+                query(node->r, m + 1, e, l, r);
+    }
+};
