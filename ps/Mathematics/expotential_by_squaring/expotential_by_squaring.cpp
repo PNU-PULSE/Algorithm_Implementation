f9a3c3f6bd0ee1f601d1b58592c7bc8e0c52db4b
pnuth <85662273+radicalparty@users.noreply.github.com>
2024-08-14 11:19:27 +0900

    Update expotential_by_squaring.cpp

--- ps/Mathematics/expotential_by_squaring/expotential_by_squaring.cpp
+++ ps/Mathematics/expotential_by_squaring/expotential_by_squaring.cpp
@@ -1,3 +1,4 @@
+//tip: python에서는 pow(n, k, p): n^k mod p를 최적화해서 계산해주므로 참고하자. python에서 pow(n, k) % p는 pow(n, k, p)와 다르게 느리므로 주의
 using ll = long long;
 ll ebs(ll v, ll p, ll mod){
     if (p == 1)  return v;
